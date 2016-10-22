f42993e68412da8795612eb786daad20ff305353
Fernando Chimicoviaki <f.nascimento@avance-authent.com.br>
2016-10-22 15:21:13 -0200

    write first app :memo:

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ RUN mkdir /angular-first
 ADD . /angular-first
 WORKDIR /angular-first/angular_first/
 RUN npm install
-RUN npm typings install
+RUN npm run typings install
---
+++ angular_first/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  imports: [ BrowerModule ],
+  declarations: [ AppComponent ],
+  bootstrap: [ AppComponent ],
+})
+
+export class AppModule { }
+
