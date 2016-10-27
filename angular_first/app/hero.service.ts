41d7c1aac9f311c8e4c9aec87ddd627e5bfc43c1
Fernando Chimicoviaki <f.nascimento@avance-authent.com.br>
2016-10-27 16:06:49 -0200

    Finish tutorial :trophy:

--- angular_first/app/hero.service.ts
+++ angular_first/app/hero.service.ts
@@ -42,6 +42,14 @@ export class HeroService{
       .catch(this.handleError);
   }
 
+  delete(id: number): Promise<void> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete(url, {headers: this.headers})
+      .toPromise()
+      .then(() => null)
+      .catch(this.handleError);
+  }
+
   private handleError(error: any): Promise<any> {
     console.error('An error occurred', error);
     return Promise.reject(error.message || error);
